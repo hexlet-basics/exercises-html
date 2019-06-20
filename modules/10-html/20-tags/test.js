1a5d7a5760aafda58f2270d4951f2cc385d9ca37
Kirill Mokevnin <mokevnin@gmail.com>
2019-06-20 16:35:29 -0400

    add tests

--- modules/10-html/20-tags/test.js
+++ modules/10-html/20-tags/test.js
@@ -1,1 +1,9 @@
--
+const fs = require('fs');
+const q = require('@testing-library/dom');
+
+test('check html', () => {
+  const html = fs.readFileSync(`${__dirname}/index.html`).toString();
+  const container = document.createElement('div');
+  container.innerHTML = html;
+  expect(q.getByText(container, /съешь/i, { selector: 'p' })).toBeVisible();
+});
--- modules/20-hypertext/20-text-style/test.js
+++ modules/20-hypertext/20-text-style/test.js
@@ -1,1 +1,10 @@
--
+const fs = require('fs');
+const q = require('@testing-library/dom');
+
+test('check html', () => {
+  const html = fs.readFileSync(`${__dirname}/index.html`).toString();
+  const container = document.createElement('div');
+  container.innerHTML = html;
+  expect(q.getByText(container, /жирный/i, { selector: 'p > b' })).toBeVisible();
+  expect(q.getByText(container, /наклон/i, { selector: 'p > em' })).toBeVisible();
+});
