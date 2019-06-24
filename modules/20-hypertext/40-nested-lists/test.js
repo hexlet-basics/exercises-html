497be56f7e811ab0e811589029c5258eeb793134
n.mikhaylov <nmikhaylov92@gmail.com>
2019-06-24 15:50:45 +0300

    add test in 40-nested-lists

--- modules/20-hypertext/40-nested-lists/test.js
+++ modules/20-hypertext/40-nested-lists/test.js
@@ -1,1 +1,20 @@
--
+const fs = require('fs');
+const q = require('@testing-library/dom');
+
+const html = fs.readFileSync(`${__dirname}/index.html`).toString();
+const container = document.createElement('div');
+container.innerHTML = html;
+
+
+test('numeric lists', () => {
+  const li = container.querySelectorAll('ol > li');
+
+  expect(li).toHaveLength(2);
+});
+
+test('nested lists', () => {
+  const ul = container.querySelector('ol > li > ul');
+  const li = ul.querySelectorAll('li');
+
+  expect(li).toHaveLength(2);
+});
