654014680f99f6a143958e2118d53a72df8ba1a9
n.mikhaylov <nmikhaylov92@gmail.com>
2019-06-24 15:24:06 +0300

    add exercises and test in 20-text-style

--- modules/20-hypertext/20-text-style/test.js
+++ modules/20-hypertext/20-text-style/test.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
 const q = require('@testing-library/dom');
 
-test('check html', () => {
-  const html = fs.readFileSync(`${__dirname}/index.html`).toString();
-  const container = document.createElement('div');
-  container.innerHTML = html;
-  expect(q.getByText(container, /жирный/i, { selector: 'p > b' })).toBeVisible();
-  expect(q.getByText(container, /наклон/i, { selector: 'p > em' })).toBeVisible();
+const html = fs.readFileSync(`${__dirname}/index.html`).toString();
+const container = document.createElement('div');
+container.innerHTML = html;
+
+test('paragraph', () => {
+  const p = container.querySelector('p');
+  expect(p).toBeVisible();
+});
+
+test('bold', () => {
+  expect(q.getByText(container, 'курсы', { selector: 'b' })).toBeVisible();
+});
+
+test('italics', () => {
+  expect(q.getByText(container, 'по программированию', { selector: 'em' })).toBeVisible();
 });
