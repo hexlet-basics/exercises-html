571fd71c538ed40f19e8b7b415218f540bd07a96
n.mikhaylov <nmikhaylov92@gmail.com>
2019-06-24 18:15:52 +0300

    add test in 70-special-characters

--- modules/20-hypertext/70-special-characters/test.js
+++ modules/20-hypertext/70-special-characters/test.js
@@ -1,1 +1,14 @@
--
+const fs = require('fs');
+const q = require('@testing-library/dom');
+
+const html = fs.readFileSync(`${__dirname}/index.html`).toString();
+const container = document.createElement('div');
+container.innerHTML = html;
+
+
+test('special-characters 1/4', () => {
+  const char = html.trim();
+  const isCorrectChar = char === '&frac14;';
+
+  expect(isCorrectChar).toBe(true);
+});
