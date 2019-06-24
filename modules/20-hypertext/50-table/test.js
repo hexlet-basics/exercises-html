e76091fa71597852c0c6f846f0528b0d9c63eaeb
n.mikhaylov <nmikhaylov92@gmail.com>
2019-06-24 16:00:24 +0300

    add test in 50-table

--- modules/20-hypertext/50-table/test.js
+++ modules/20-hypertext/50-table/test.js
@@ -1,1 +1,21 @@
--
+const fs = require('fs');
+const q = require('@testing-library/dom');
+
+const html = fs.readFileSync(`${__dirname}/index.html`).toString();
+const container = document.createElement('div');
+container.innerHTML = html;
+
+
+test('head table', () => {
+  const thead = container.querySelector('table > thead');
+  const th = thead.querySelectorAll('th');
+
+  expect(th).toHaveLength(3);
+});
+
+test('body table', () => {
+  const tbody = container.querySelector('table > tbody');
+  const td = tbody.querySelectorAll('td');
+
+  expect(td).toHaveLength(3);
+});
