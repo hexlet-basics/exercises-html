76f96240809abc9fab07e9f87b0d1230250ab206
n.mikhaylov <nmikhaylov92@gmail.com>
2019-06-24 15:34:52 +0300

    add exercises and test in 30-lists

--- modules/20-hypertext/30-lists/test.js
+++ modules/20-hypertext/30-lists/test.js
@@ -1,1 +1,17 @@
--
+const fs = require('fs');
+const q = require('@testing-library/dom');
+
+const html = fs.readFileSync(`${__dirname}/index.html`).toString();
+const container = document.createElement('div');
+container.innerHTML = html;
+
+
+test('numeric lists', () => {
+  const ol = container.querySelector('ol');
+  expect(ol).toBeVisible();
+});
+
+test('count li', () => {
+  const li = container.querySelectorAll('li');
+  expect(li).toHaveLength(5);
+});
