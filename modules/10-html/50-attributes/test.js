9ad33bb81c8bac3d0947076fc9e015c729557e84
n.mikhaylov <nmikhaylov92@gmail.com>
2019-06-25 23:02:51 +0300

    add 50-attributes lesson

---
+++ modules/10-html/50-attributes/test.js
@@ -0,0 +1,6 @@
+const { test } = require('tests');
+
+test(({ query, expect }) => {
+  const elements = query(document, 'p', HTMLElement);
+  expect(elements).to.have.attr('title').match(/,*/);
+});
