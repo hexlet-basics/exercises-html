052a49345fb63564bcc671337d2e167936ccc833
n.mikhaylov <nmikhaylov92@gmail.com>
2019-06-25 11:39:31 +0300

    add test for 80-links lesson

--- modules/20-hypertext/80-links/test.js
+++ modules/20-hypertext/80-links/test.js
@@ -1,6 +1,6 @@
 const { test } = require('tests');
 
 test(({ query, expect }) => {
-  // const element = query(document, 'p', HTMLParagraphElement);
-  // expect(element).to.contain.text('мягких');
+  const element = query(document, 'a');
+  expect(element).to.have.attr('href', 'https://code-basics.ru/');
 });
