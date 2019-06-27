4da17622c5a2b36b8bfc567e014bff26c4cf7d10
n.mikhaylov <nmikhaylov92@gmail.com>
2019-06-27 17:15:51 +0300

    add test in 60-colspan-rowspan

--- modules/20-hypertext/60-colspan-rowspan/test.js
+++ modules/20-hypertext/60-colspan-rowspan/test.js
@@ -1,6 +1,12 @@
 const { test } = require('tests');
 
-test(({ query, expect }) => {
-  // const element = query(document, 'p', HTMLParagraphElement);
-  // expect(element).to.contain.text('мягких');
+test(({ querySelectorAll, query, expect }) => {
+  const trs = querySelectorAll(document, 'table > tbody > tr', HTMLElement);
+  expect(trs).to.have.length(2);
+
+  const tds = querySelectorAll(document, 'table > tbody > tr:nth-child(2) > td', HTMLElement);
+  expect(tds).to.have.length(2);
+
+  const tdWithColspan = query(document, 'table > tbody > tr:nth-child(2) > td:nth-child(1)', HTMLElement);
+  expect(tdWithColspan).to.have.attribute('colspan', '2');
 });
