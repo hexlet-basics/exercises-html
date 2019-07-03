c6d18c3c250936290350d7cf55e86ea2a8accacf
n.mikhaylov <nmikhaylov92@gmail.com>
2019-07-03 16:30:17 +0300

    fix test in 10-basics

--- modules/40-structure/10-basics/test.js
+++ modules/40-structure/10-basics/test.js
@@ -1,5 +1,8 @@
 const { test } = require('tests');
 
 test(({ expect, html }) => {
+  expect(html).to.contain('<!DOCTYPE html>');
   expect(html).to.contain('lang');
+  expect(html).to.contain('<head>');
+  expect(html).to.contain('<body>');
 });
