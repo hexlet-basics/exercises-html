55869255002010b678f03b0d235959871acdc949
n.mikhaylov <nmikhaylov92@gmail.com>
2019-07-04 14:51:32 +0300

    add test in 50-aside

--- modules/50-semantics/50-aside/test.js
+++ modules/50-semantics/50-aside/test.js
@@ -1,7 +1,14 @@
 const { test } = require('tests');
 
 test(({ query, expect }) => {
-  const header = query(document, 'header');
+  console.log(document.documentElement.innerHTML);
 
+  const header = query(document, 'header');
   expect(header).to.be.visible;
+
+  const aside = query(document, 'aside');
+  expect(aside).to.be.visible;
+
+  const main = query(document, 'main');
+  expect(main).to.be.visible;
 });
