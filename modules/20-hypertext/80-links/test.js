6a3697e9c39f242deab9fd77df308ad82c1ad8ee
n.mikhaylov <nmikhaylov92@gmail.com>
2019-07-10 11:23:57 +0300

    fix errors in text

--- modules/20-hypertext/80-links/test.js
+++ modules/20-hypertext/80-links/test.js
@@ -2,5 +2,5 @@ const { test } = require('tests');
 
 test(({ query, expect }) => {
   const element = query(document, 'a');
-  expect(element).to.have.attr('href', 'https://code-basics.ru/');
+  expect(element).to.have.attr('href', 'https://code-basics.ru');
 });
