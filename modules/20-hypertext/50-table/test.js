01e7269678d0f392af56a7509037b43bbe42ee22
n.mikhaylov <nmikhaylov92@gmail.com>
2019-07-15 16:05:21 +0300

    fix mistakes in 20-hypertext module

--- modules/20-hypertext/50-table/test.js
+++ modules/20-hypertext/50-table/test.js
@@ -3,8 +3,11 @@ const { test } = require('tests');
 test(({ querySelectorAll, query, expect }) => {
   query(document, 'table > thead', HTMLTableHeaderCellElement);
   const ths = querySelectorAll(document, 'table > thead > tr > th', HTMLElement);
-  expect(ths).to.have.length(3);
+  expect(ths).to.have.length(2);
+
+  const trs = querySelectorAll(document, 'table > tbody > tr', HTMLElement);
+  expect(trs).to.have.length(2);
 
   const tds = querySelectorAll(document, 'table > tbody > tr > td', HTMLElement);
-  expect(tds).to.have.length(3);
+  expect(tds).to.have.length(4);
 });
