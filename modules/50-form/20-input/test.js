59ad244af6fcc80aac88a00be545f7e56380aa1d
n.mikhaylov <nmikhaylov92@gmail.com>
2019-07-27 16:01:20 +0300

    add 20-input lesson

---
+++ modules/50-form/20-input/test.js
@@ -0,0 +1,9 @@
+const { test } = require('tests');
+
+test(({ query, expect }) => {
+  const form = query(document, 'form');
+  const inputs = form.querySelectorAll('input');
+
+  expect(form).to.have.attr('action', '/people.php');
+  expect(inputs).to.have.length(2);
+});
