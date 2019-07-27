a241e0b3e71a6a92ce92200839dce580b1a117e9
n.mikhaylov <nmikhaylov92@gmail.com>
2019-07-27 16:59:19 +0300

    fix text in 10-basics lesson

--- modules/50-form/10-basics/test.js
+++ modules/50-form/10-basics/test.js
@@ -3,5 +3,5 @@ const { test } = require('tests');
 test(({ query, expect }) => {
   const form = query(document, 'form');
 
-  expect(form).to.have.attr('action', '/people.php');
+  expect(form).to.have.attr('action', '/people');
 });
