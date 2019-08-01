30aacceea71307b2af85cc2ccf9b96a78a076d35
n.mikhaylov <nmikhaylov92@gmail.com>
2019-08-01 12:31:40 +0300

    add 30-checkbox lesson

---
+++ modules/50-form/30-checkbox/test.js
@@ -0,0 +1,14 @@
+const { test } = require('tests');
+
+test(({ query, expect }) => {
+  const form = query(document, 'form');
+  const labels = form.querySelectorAll('label');
+
+  expect(form).to.have.attr('action', '/people');
+  expect(labels).to.have.length(2);
+
+  labels.forEach((label) => {
+    const input = label.querySelector('input');
+    expect(input).to.have.attr('type', 'checkbox');
+  });
+});
