29815b0940fbaf06d31bdc7b2ff384286b848e3b
n.mikhaylov <nmikhaylov92@gmail.com>
2019-08-20 11:06:25 +0300

    fix exercises in 60-colspan-rowspan lesson

--- modules/20-hypertext/60-colspan-rowspan/test.js
+++ modules/20-hypertext/60-colspan-rowspan/test.js
@@ -2,11 +2,14 @@ const { test } = require('tests');
 
 test(({ querySelectorAll, query, expect }) => {
   const trs = querySelectorAll(document, 'table > tbody > tr', HTMLElement);
-  expect(trs).to.have.length(2);
+  expect(trs).to.have.length(1);
 
-  const tds = querySelectorAll(document, 'table > tbody > tr:nth-child(2) > td', HTMLElement);
+  const trHead = querySelectorAll(document, 'table > thead > tr');
+  expect(trHead).to.have.length(1);
+
+  const tds = querySelectorAll(document, 'table > tbody > tr > td', HTMLElement);
   expect(tds).to.have.length(2);
 
-  const tdWithColspan = query(document, 'table > tbody > tr:nth-child(2) > td:nth-child(1)', HTMLElement);
+  const tdWithColspan = query(document, 'table > tbody > tr > td:nth-child(1)', HTMLElement);
   expect(tdWithColspan).to.have.attribute('colspan', '2');
 });
