057cb43270dfff3d6ae9bc186479b027372c051a
n.mikhaylov <nmikhaylov92@gmail.com>
2019-08-22 12:28:02 +0300

    add 20-header lesson

---
+++ modules/45-semantics/20-header/test.js
@@ -0,0 +1,16 @@
+const { test } = require('tests');
+
+test(({ query, querySelectorAll, expect }) => {
+
+  const header = query(document, 'header');
+  expect(header).to.be.visible;
+
+  const img = query(header, 'img');
+  expect(img).to.be.visible;
+
+  const ul = query(header, 'ul');
+  expect(ul).to.be.visible;
+
+  const li = querySelectorAll(ul, 'li');
+  expect(li).to.be.length(2);
+});
