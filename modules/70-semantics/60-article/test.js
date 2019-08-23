58e964117db4aa0367ad0f7293b84ee08e29e3f7
n.mikhaylov <nmikhaylov92@gmail.com>
2019-08-23 12:50:28 +0300

    add 60-article lesson

---
+++ modules/70-semantics/60-article/test.js
@@ -0,0 +1,36 @@
+const { test } = require('tests');
+
+test(({ query, querySelectorAll, expect }) => {
+
+  const header = query(document, 'header');
+  expect(header).to.be.visible;
+
+  const img = query(header, 'img');
+  expect(img).to.be.visible;
+
+  const nav = query(header, 'nav');
+  expect(nav).to.be.visible;
+
+  const ul = query(nav, 'ul');
+  expect(ul).to.be.visible;
+
+  const li = querySelectorAll(ul, 'li');
+  expect(li).to.be.length(2);
+
+  const main = query(document, 'main');
+  expect(main).to.be.visible;
+
+  const section = query(main, 'section');
+  expect(section).to.be.visible;
+
+  const h2 = query(section, 'h2');
+  expect(h2).to.be.visible;
+
+  const articles = querySelectorAll(section, 'article');
+  expect(articles).to.be.length(2);
+
+  articles.forEach((article) => {
+    const h3 = query(article, 'h3');
+    expect(h3).to.be.visible;
+  });
+});
