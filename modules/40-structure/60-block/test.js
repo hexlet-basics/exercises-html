123a9d1ea93baba098e0bc0accf82f63e45abad5
n.mikhaylov <nmikhaylov92@gmail.com>
2019-10-15 15:06:57 +0300

    fix typo

--- modules/40-structure/60-block/test.js
+++ modules/40-structure/60-block/test.js
@@ -1,7 +1,7 @@
 const { test } = require('tests');
 
 test(({ query, expect }) => {
-  const links = document.querySelectorAll('a');
+  const links = document.querySelectorAll('span');
 
   expect(links).to.be.length(2);
 });
