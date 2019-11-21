6563f643b800168a51431f02298738bc9b6701ac
n.mikhaylov <nmikhaylov92@gmail.com>
2019-11-21 11:02:37 +0300

    rewrite test in 20-audio lesson

--- modules/30-media/20-audio/test.js
+++ modules/30-media/20-audio/test.js
@@ -2,6 +2,6 @@ const { test } = require('tests');
 
 test(({ query, expect }) => {
   const element = query(document, 'audio');
-  expect(query(element, 'source:nth-child(1)')).to.have.attr('src', 'https://www.w3schools.com/html/horse.ogg');
-  expect(query(element, 'source:nth-child(2)')).to.have.attr('src', 'https://www.w3schools.com/html/horse.mp3');
+  expect(query(element, 'source:nth-child(1)')).to.exist;
+  expect(query(element, 'source:nth-child(2)')).to.exist;
 });
