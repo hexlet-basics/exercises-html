7c0872dd2b37a94a2f2a38f1570e95413403f199
n.mikhaylov <nmikhaylov92@gmail.com>
2020-01-15 10:08:49 +0300

    add tests in 20-microdata lesson

--- modules/60-other/20-microdata/test.js
+++ modules/60-other/20-microdata/test.js
@@ -1,10 +1,13 @@
 const { test } = require('tests');
 
-test(({ query, expect }) => {
+test(({ query, expect, html }) => {
 
   const name = query(document, '[itemprop="name"]');
   expect(name).to.be.visible;
 
   const email = query(document, '[itemprop="email"]');
   expect(email).to.be.visible;
+
+  expect(html).to.contain('itemscope');
+  expect(html).to.contain('itemtype="http://schema.org/Organization"');
 });
