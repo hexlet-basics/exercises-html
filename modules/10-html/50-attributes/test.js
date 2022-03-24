6b6638c1070c92ee5ed4f8da5aa20528e6c1b2ec
n.mikhaylov <nmikhaylov92@gmail.com>
2022-03-24 16:12:54 +0300

    refactor first module

--- modules/10-html/50-attributes/test.js
+++ modules/10-html/50-attributes/test.js
@@ -3,5 +3,5 @@ const { test } = require('tests');
 test(({ query, expect }) => {
   const elements = query(document, 'p', HTMLElement);
   expect(elements).to.have.attr('title').match(/,*/);
-  expect(elements).to.have.attr('align', 'right');
+  expect(elements).to.have.attr('class', 'hexlet');
 });
