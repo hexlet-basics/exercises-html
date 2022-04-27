c6198be117f17c3e756482f0c7231e1bf2de14d7
Yaroslav Mikhaylov <sponaproz@gmail.com>
2022-04-27 20:35:39 +0300

    fix empty modules (#147)
    * fixes
    
    * fix test

--- modules/10-html/20-tags/test.js
+++ modules/10-html/20-tags/test.js
@@ -2,5 +2,5 @@ const { test } = require('tests');
 
 test(({ query, expect }) => {
   const element = query(document, 'p', HTMLParagraphElement);
-  expect(element).to.contain.text('мягких');
+  expect(element).to.contain.text('lazy');
 });
--- modules/20-hypertext/20-text-style/test.js
+++ modules/20-hypertext/20-text-style/test.js
@@ -2,8 +2,8 @@ const { test } = require('tests');
 
 test(({ query, expect }) => {
   const b = query(document, 'p > b', HTMLElement);
-  expect(b).to.have.text('курсы');
+  expect(b).to.have.text('courses');
 
   const em = query(document, 'p > em', HTMLElement);
-  expect(em).to.have.text('по программированию');
+  expect(em).to.have.text('programming');
 });
