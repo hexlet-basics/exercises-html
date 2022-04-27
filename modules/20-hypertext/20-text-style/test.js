cd3978d705a90997baebfa8ec1e24d57da7bbeef
Yaroslav Mikhaylov <sponaproz@gmail.com>
2022-04-27 18:03:31 +0300

    fix test

--- modules/20-hypertext/20-text-style/test.js
+++ modules/20-hypertext/20-text-style/test.js
@@ -2,7 +2,7 @@ const { test } = require('tests');
 
 test(({ query, expect }) => {
   const b = query(document, 'p > b', HTMLElement);
-  expect(b).to.have.text('курсы');
+  expect(b).to.have.text('courses');
 
   const em = query(document, 'p > em', HTMLElement);
   expect(em).to.have.text('programming');
